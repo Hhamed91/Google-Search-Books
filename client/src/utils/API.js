48f2974a42f6c719f4cdc6bcb64b248f83493170
Hazem Hamed <hazemhamed@hazemhamed-mbp.corp.dropbox.com>
2019-11-16 14:40:07 -0800

    clean out the api routes in utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,29 @@
-import axios from "axios";
-
+import axios from "axios"
 
 export default {
-    getGoogleSearchBooks: function(query){
+    // Get book from google search 
+    getGoogleSearchBooks: function(query) {
         return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
-    }
+    },
 
-    getBooks:function(){
+    // Gets all books
+    getBooks: function () {
         return axios.get("/api/books");
-    }
+    },
 
-    getBooks:function(){
+    // Gets the book with the given id
+    getBook: function (id) {
         return axios.get("/api/books/" + id);
+    },
+
+    // Saves a book to the database
+    saveBook: function (savedBooks) {
+        return axios.post("/api/books", savedBooks);
+    },
+
+    // Deletes the book with the given id
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id);
     }
 
-    savebooks:function(){
-        return axios.post("/api/books", this.savebooks)
-    }
 }
